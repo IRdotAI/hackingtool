b9b488553f28d6659c4f0062fe3b73477952c843
AbrasiveTax49 <71939466+AbrasiveTax49@users.noreply.github.com>
2022-09-20 21:42:49 +0500

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,52 @@
+echo "    .aMMMb  dMMMMb  dMMMMb  .aMMMb  .dMMMb  dMP dMP dMP dMMMMMP dMMMMMMP .aMMMb  dMP dMP     ";
+echo "   dMP.dMP dMP.dMP dMP.dMP dMP.dMP dMP. VP amr dMP dMP dMP        dMP   dMP.dMP dMK.dMP      ";
+echo "  dMMMMMP dMMMMK. dMMMMK. dMMMMMP  VMMMb  dMP dMP dMP dMMMP      dMP   dMMMMMP .dMMMK.       ";
+echo " dMP dMP dMP.aMF dMP'AMF dMP dMP dP .dMP dMP  YMvAP' dMP        dMP   dMP dMP dMP'AMF        ";
+echo "dMP dMP dMMMMP' dMP dMP dMP dMP  VMMMP' dMP    VP'  dMMMMMP    dMP   dMP dMP dMP dMP         ";
+echo "                                                                                             ";
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/hackingtool/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/hackingtool
+
+clear
+
+mkdir hackingtool
+
+clear
+
+cd hackingtool
+
+clear
+
+git clone https://github.com/AbrasiveTax49/hackingtool.git
+
+clear
+
+cd hackingtool
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
